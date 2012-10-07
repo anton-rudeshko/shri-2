c0b9abcafc8973206b13079112f811a332c264c7
Anton Rudeshko <anton@rudeshko.com>
2012-10-07 08:47:01 -0700

    Replacing quotes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-require(["models/event"], function (Event) {
-  TestCase("Event model test", {
-    "test Event should be function": function () {
+require(['models/event'], function (Event) {
+  TestCase('Event model test', {
+    'test Event should be function': function () {
       assertFunction(Event);
     }
   });
