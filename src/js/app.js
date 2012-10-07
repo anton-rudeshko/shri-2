d2a1bc3fb3e60f7eb75cff8736147bf7a37f7653
Anton Rudeshko <anton@rudeshko.com>
2012-10-07 03:32:57 -0700

    Added backbone library

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,16 @@
 require.config({
   paths: {
-    'jquery': 'lib/jquery'
+    'jquery': 'lib/jquery',
+    'underscore': 'lib/underscore',
+    'backbone': 'lib/backbone'
+  },
+  shim: {
+    jquery: { exports: '$' },
+    underscore: { exports: '_' },
+    backbone: { exports: 'Backbone', deps: ['underscore', 'jquery'] }
   }
 });
 
-define("app", ["jquery"], function ($) {
-    $('.loading').html('Загружено');
+define("app", ["jquery", "backbone"], function ($, Backbone) {
+  $('.loading').html('Загружено, backbone v.' + Backbone.VERSION);
 });
