475b681b402c4bcc599c0f01e38748f626cc348b
Anton Rudeshko <anton@rudeshko.com>
2012-10-12 22:02:44 +0400

    Renaming test

--- test/EventModel.test.js
+++ test/EventModel.test.js
@@ -1,15 +1,15 @@
-require(['EventModel'], function (Event) {
-  TestCase('Event model test', {
-    'test Event should be function': function () {
-      assertFunction(Event);
+require(['EventModel'], function (EventModel) {
+  TestCase('EventModel test', {
+    'test EventModel should be function': function () {
+      assertFunction(EventModel);
     },
     'test should not set negative length': function () {
-      var event = new Event();
+      var event = new EventModel();
       event.set('length', -10);
       assertEquals(3600000, event.get('length'));
     },
     'test should not set invalid title': function () {
-      var event = new Event({title: 'bla-bla'});
+      var event = new EventModel({title: 'bla-bla'});
       event.set('title', '');
       assertEquals('bla-bla', event.get('title'));
     }
