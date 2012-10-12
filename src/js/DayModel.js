a808e0789dd3a78c06801f1b5d1bd2a7cfeb116e
Anton Rudeshko <anton@rudeshko.com>
2012-10-12 23:01:33 +0400

    Removing underscore dependency

--- src/js/DayModel.js
+++ src/js/DayModel.js
@@ -1,4 +1,4 @@
-define('DayModel', ['backbone', 'underscore', 'EventCollection', 'EventModel'], function (Backbone, _, EventCollection, EventModel) {
+define('DayModel', ['backbone', 'EventCollection', 'EventModel'], function (Backbone, EventCollection, EventModel) {
   var DAYS = ['', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
   return Backbone.Model.extend({
     defaults: {
@@ -10,12 +10,11 @@ define('DayModel', ['backbone', 'underscore', 'EventCollection', 'EventModel'], 
 
     initialize: function () {
       this.set('title', DAYS[this.get('date').getDay()]);
-      _.bindAll(this, 'checkEmpty');
-      var eventModels = _(this.get('events')).map(function (event) {
+      var eventModels = this.get('events').map(function (event) {
         return new EventModel(event);
       });
       this.events = new EventCollection(eventModels);
-      this.events.on('change', this.checkEmpty);
+      this.events.on('change', this.checkEmpty, this);
 
       this.checkEmpty();
     },
