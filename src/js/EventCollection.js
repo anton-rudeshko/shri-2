8d5d4e8c3db4a4905b18fd92248ad0f7cd6bcee0
Anton Rudeshko <anton@rudeshko.com>
2012-10-13 19:28:37 +0400

    Adding localstorage to collections

--- src/js/EventCollection.js
+++ src/js/EventCollection.js
@@ -1,6 +1,7 @@
-define('EventCollection', ['backbone', 'EventModel'], function (Backbone, EventModel) {
+define('EventCollection', ['backbone', 'EventModel', 'backbone.localstorage'], function (Backbone, EventModel) {
   return Backbone.Collection.extend({
     model: EventModel,
+    localStorage: new Backbone.LocalStorage("EventCollection"),
 
     comparator: function (model) {
       return model.get('start').getTime();
