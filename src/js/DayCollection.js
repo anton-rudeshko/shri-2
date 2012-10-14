2fc0844557bab7f06b3672987976c8acfe83cb5a
Anton Rudeshko <anton@rudeshko.com>
2012-10-14 19:20:02 +0400

    Fixed localstorage bug

--- src/js/DayCollection.js
+++ src/js/DayCollection.js
@@ -1,7 +1,5 @@
 define('DayCollection', ['backbone', 'DayModel', 'backbone.localstorage'], function (Backbone, DayModel) {
   return Backbone.Collection.extend({
-    localStorage: new Backbone.LocalStorage("DayCollection"),
-
     model: DayModel
   });
 });
--- src/js/EventCollection.js
+++ src/js/EventCollection.js
@@ -1,7 +1,6 @@
 define('EventCollection', ['backbone', 'EventModel', 'backbone.localstorage'], function (Backbone, EventModel) {
   return Backbone.Collection.extend({
     model: EventModel,
-    localStorage: new Backbone.LocalStorage("EventCollection"),
 
     comparator: function (model) {
       return model.get('time').getTime();
